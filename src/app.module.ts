d1590f6afbaf9fe24b33183bbd003118db1742d3
mistersmith <sonc3k56@outlook.com.vn>
2024-01-15 17:06:07 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'example',
+      username: 'postgres',
+      entities: [],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from 'src/user/dto/sign-up.dto';
+import * as bcrypt from 'bcryptjs';
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRespository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { name, email, password } = signUpDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userRespository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await this.userRespository.save(user);
+
+    const token = this.jwtService.sign({
+      id: user.id
+    });
+
+    return token;
+  }
+
+
+  // async signIn(username: string, password: string): Promise<any> {
+  // const user = await this.userService.findOne(username);
+  // }
+}
