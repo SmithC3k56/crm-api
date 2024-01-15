61d9779c45b6ce8f6e6e68b826e39adb7096f823
mistersmith <sonc3k56@outlook.com.vn>
2024-01-15 17:44:08 +0700

    add authen

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post, Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from 'src/user/dto/sign-up.dto';
+import { SignInDto } from 'src/user/dto/sign-in.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authenService: AuthService) {}
+
+  @Post('/signup')
+  signup(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authenService.signup(signUpDto);
+  }
+  @Post('/signin')
+  signin(@Body() loginDto: SignInDto): Promise<{ token: string }> {
+    return this.authenService.signin(loginDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from 'src/user/dto/sign-up.dto';
 import * as bcrypt from 'bcryptjs';
-
+import { SignInDto } from 'src/user/dto/sign-in.dto';
 
 @Injectable()
 export class AuthService {
@@ -15,7 +15,7 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+  async signup(signUpDto: SignUpDto): Promise<{ token: string }> {
     const { name, email, password } = signUpDto;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await this.userRespository.create({
@@ -26,12 +26,30 @@ export class AuthService {
 
     await this.userRespository.save(user);
 
-    const token = this.jwtService.sign({
-      id: user.id
-    });
+    const token = this.jwtService.sign({ id: user.id });
 
     return token;
   }
 
+  async signin(signinDto: SignInDto): Promise<{ token: string }> {
+    const { email, password } = signinDto;
+    const user = await this.userRespository.findOne({
+      where: { email },
+    });
 
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (isPasswordMatched) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.jwtService.sign({ id: user.id });
+
+    return { token };
+  }
+  
 }
